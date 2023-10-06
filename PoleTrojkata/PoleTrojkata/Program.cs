eb41ee39c435234066eb6a818830830d251bffd9
unknown <m.miszczyszyn77@gmail.com>
2023-10-06 16:27:32 +0200

    opis

---
+++ PoleTrojkata/PoleTrojkata/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace PoleTrojkata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej, podaj wymiary trojkata to policze ci pole");
+            var listOfSides = new List<int>();
+        }
+
+        private void InputSide(List<int> listOfSides)
+        {
+            try
+            {
+                for (int i = 1; i <= 3; i++)
+                {
+                    var input = int.Parse(Console.ReadLine());
+
+                    if(input > 0)
+                    {
+                        listOfSides.Add(input);
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception();
+             }
+        }
+    }
+}
